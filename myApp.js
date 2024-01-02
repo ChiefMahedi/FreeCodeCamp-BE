7bbd90589e2d15b2e9c22d2222d6b27d0ec72cfd
Shawon <chiefmahedi@gmail.com>
2024-01-02 11:38:55 +0600

    Add task 3: Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -1,43 +1,16 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve the entire 'public' directory statically
+const publicDirectory = path.join(__dirname, 'public');
+app.use(express.static(publicDirectory));
 
 console.log('Hello World');
-app.get('/',(req, res)=>{
-    const file = __dirname + '/views/index.html'
+
+app.get('/', (req, res) => {
+    const file = path.join(__dirname, 'views', 'index.html');
     res.sendFile(file);
-})
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
