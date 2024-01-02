4a83f379f7e272a51c93ffdba797747c3551081e
Shawon <chiefmahedi@gmail.com>
2024-01-02 12:31:46 +0600

    Add task 6: Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const path = require("path");
 require('dotenv').config()
+app.use('/',(req, res, next)=>{
+    console.log(`${req.method} ${req.path} ${req.ip}`);
+    next();
+})
 const publicDirectory = path.join(__dirname, "public");
 const publicPath = "/public";
 app.use(publicPath, express.static(publicDirectory));
 
 console.log("Hello World");
-
 app.get("/", (req, res) => {
   const file = path.join(__dirname, "views", "index.html");
   res.sendFile(file);
