214cc4af50a5fe50e1d478d710ce1d382c178d78
Shawon <chiefmahedi@gmail.com>
2024-01-07 22:48:21 +0600

    Add task 9

--- myApp.js
+++ myApp.js
@@ -71,8 +71,9 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName},{age: ageToSet}, { new: true }, (err, data)=>{
+      done(null , data);
+  })
 };
 
 const removeById = (personId, done) => {
