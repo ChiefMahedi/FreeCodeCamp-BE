f35050ddc57d562edbd5b101490820f485a2aefc
Shawon <chiefmahedi@gmail.com>
2024-01-07 22:01:57 +0600

    Add task 5

--- myApp.js
+++ myApp.js
@@ -41,7 +41,10 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name: personName}, function(err, data){
+    done(null , data);
+  })
+
 };
 
 const findOneByFood = (food, done) => {
